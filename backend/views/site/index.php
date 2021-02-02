d719b1d9f0be3cda4b68c2d9810cceabcbd90783
ilyastuit <ilyastuit@gmail.com>
2021-02-02 18:25:50 +0500

    Customized template

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -5,10 +5,6 @@ $this->params['breadcrumbs'] = [['label' => $this->title]];
 <div class="container-fluid">
     <div class="row">
         <div class="col-lg-6">
-            <?= \hail812\adminlte3\widgets\Alert::widget([
-                'type' => 'success',
-                'body' => '<h3>Congratulations!</h3>',
-            ]) ?>
             <?= \hail812\adminlte3\widgets\Callout::widget([
                 'type' => 'danger',
                 'head' => 'I am a danger callout!',
